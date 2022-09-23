a5f2afb1406beeb82e6df9dddb01ad2a4bceafab
RomaPLETiuk <108412429+RomaPLETiuk@users.noreply.github.com>
2022-09-23 22:02:42 +0300

    Add files via upload

---
+++ laba_5/pletiuk_5.cpp
@@ -0,0 +1,98 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <conio.h>
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+ 
+using namespace std;
+ 
+class Kvadrat
+{
+private:
+    double side;
+    double area;
+    
+public:
+ 
+    Kvadrat()
+    {
+        this->side = 0.0;
+        this->area = side*side;
+    }
+ 
+    Kvadrat(double side)
+    {
+       this->side = side;
+       this->area = pow(side,2);
+    }
+    
+    double getSide() {
+            return side; 
+        }
+    double getArea() {
+            return area; 
+        }
+     
+ 
+    void setSide(double side){
+            this->side=side;
+        }
+        
+        
+   Kvadrat operator +(Kvadrat z) {
+       
+       double S = this->area+z.area;
+       
+       double side = sqrt(S);
+       Kvadrat newKvadrat(side);
+       return newKvadrat;
+   }    
+        
+        
+    
+   Kvadrat operator ++()// префіксна форма інкременту 
+   {
+
+    side++; 
+    return *this;
+    }
+ 
+   Kvadrat operator ++(int notused) // постфіксна форма інкременту
+	{
+		Kvadrat temp = *this;
+	    side++;
+		return temp;
+	}
+	
+    friend ostream& operator <<(ostream& t, Kvadrat a);
+    
+};  
+
+
+    ostream& operator <<(ostream& t, Kvadrat a)
+	{
+		t << "side =" << a.side << ", area =" << a.area << endl;
+		return t;
+	}
+
+
+ 
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+ 
+   Kvadrat a(5), b(4), c;
+   // a = ++b;
+   ++b;
+   cout<<b;
+   c=a+b;
+   cout <<c;
+    return 0;
+}
