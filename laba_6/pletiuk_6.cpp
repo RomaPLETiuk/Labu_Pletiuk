a17e4a891400e9f1d07b2983b09573e9bb9be782
RomaPLETiuk <108412429+RomaPLETiuk@users.noreply.github.com>
2022-09-30 19:16:51 +0300

    Add files via upload

---
+++ laba_6/pletiuk_6.cpp
@@ -0,0 +1,150 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+class PrintEdition
+{
+protected:
+  int year;  
+  string name; 
+public:
+
+   void setYear(int year) { 
+       this->year = year; 
+       
+   }
+   
+   int getYear() {
+       return year;
+       }
+       
+   void setName(string text) { name = text; }
+   
+   string getName() { 
+       return name; 
+       
+   }
+};
+
+class Book : public PrintEdition
+{
+  
+  string autor; 
+  
+public:
+  Book(){}
+  ~Book(){}
+  void setAutor(string text) { autor = text; }
+  string getAutor() { return autor; }
+void show ();
+};
+
+class Journal : public PrintEdition
+{
+protected:
+   int day;
+   int moth;
+public:
+   Journal(){}
+   ~Journal(){}
+
+   void setDay(int day) { 
+       this->day = day; 
+       
+   }
+   int getDay() { return day; }
+   
+   void setMoth (int moth) { 
+       this->moth = moth; 
+       
+   }
+   int getMoth() { return moth; }
+void show ();
+};
+
+
+class Edubook : public Book
+{
+    
+    string subject;
+    int klas;
+ public:   
+   Edubook(){
+       
+   }
+   
+   ~Edubook(){
+       
+   }
+   
+  
+   void setKlas (int klas) { 
+       this->klas = klas; 
+       
+   }
+   int getKlas() { return klas; }
+   
+   void setSubject(string text) { subject = text; }
+   string getSubject() { return subject; }
+   void show (); 
+    
+    
+};
+
+
+void Book::show()
+{
+cout<<"Книга: "<<" "<<getName()<<endl;
+cout<<"Рік виходу: "<<getYear()<<"р. \n";
+cout<<"Автор: "<<autor<<endl<<endl;
+}
+
+void Journal::show()
+{
+cout<<"Журнал: "<<getName()<<endl;
+cout<<"Дата виходу: "<<getYear() <<"р., "<<moth<<" місяця, "<<day<<" дня."<<endl<<endl;
+}
+
+
+void Edubook::show(){
+    
+cout<<"Підручник: "<<getName()<<endl;
+cout<<"Року виходу "<<getYear() <<"р. "<<endl;
+cout<<"З предмету: "<<subject<<endl;
+cout<<"Для учнів "<<klas<<" класу"<<endl<<endl;
+    
+}
+
+int main()
+{
+  setlocale(0, "Ukr")  
+Book b;
+Journal j;
+Edubook e;
+b.setName("Капітан на острові");
+b.setYear(2002);
+b.setAutor("Тарас Шевченко");
+
+j.setMoth (12);
+j.setName("Шишкін лес");
+j.setYear(2021);
+j.setDay(28);
+
+e.setName("Історія");
+e.setYear(1995);
+e.setSubject("Історія України");
+e.setKlas (5);
+
+b.show();
+j.show();
+e.show();
+}
